b1366893a354592ca71f547fa7a352d095994afc
Akib Ali <akib7599@gmail.com>
2024-11-13 22:41:15 -0800

    used api

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,62 @@
 import Image from "next/image";
 import styles from "./page.module.css";
+import axios from "axios";
+import Link from "next/link";
 
-export default function Home() {
-  return (
-    <div className={styles.page}>
-     <h1>Next App try to deployment</h1>
-     <p>try to deployment</p>
-    </div>
+async function productslist() {
+  try {
+      const responce = await axios.get("http://localhost:3000/api/products" )
+      return responce.data.result
+
+
+  } catch (error) {
+      console.log(error)
+
+  }
+}
+
+
+export default async function Home() {
+
+ 
+
+const data = await productslist()
+console.log("product list data", data)
+
+
+
+
+
+return (
+    <>
+        <h1> All Products here</h1>
+
+        <table border="1" style={{ width: "70%" }}>
+            <thead>
+                <tr>
+                    <th>Name</th>
+                    <th>Email</th>
+                    <th>Age</th>
+                    <th>Action</th>
+                </tr>
+            </thead>
+            <tbody>
+                {data?.map((elem, index) => {
+                    return (
+                        <tr key={index}>
+                            <td>{elem.name}</td>
+                            <td>{elem.email}</td>
+                            <td>{elem.age} </td>
+                            <td>
+                                <Link href={`products/${elem._id}`}> Update </Link>
+                                
+                            </td>
+                        </tr>
+                    )
+                })}
+            </tbody>
+        </table>
+    </>
+
   );
 }
